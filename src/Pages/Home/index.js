396382149f7e1df5bff974e8c188d290b3db8a0b
lapthuan <lapthuan0805@gmail.com>
2024-05-23 20:18:29 +0700

    oke

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -4,15 +4,34 @@ import chuong from "../../Image/chuong.png"
 import chuonggif from "../../Image/chuong.gif"
 import imgbaochay from "../../Image/baochao.png"
 import mua from "../../Image/mua.gif"
-import { Alert, Input, Typography, message } from "antd"
+import { Alert, Input, Table, Typography, message } from "antd"
 import LiquidFillGauge from "react-liquid-gauge"
 import { useEffect, useState } from "react"
 import { database } from "../../connect/firebase"
 import { onValue, ref, set } from "firebase/database"
+import { format } from 'date-fns';
 
-
+const columns = [
+    {
+        title: 'No',
+        dataIndex: 'no',
+        key: 'no',
+    },
+    {
+        title: 'Timestamp',
+        dataIndex: 'timestamp',
+        key: 'timestamp',
+        render: (text) => format(new Date(text), 'dd/MM/yyyy HH:mm:ss'),
+    },
+    {
+        title: 'Fire Status',
+        dataIndex: 'fire',
+        key: 'fire',
+    },
+];
 const Home = () => {
     const [data, setData] = useState({})
+    const [dataTable, setDataTable] = useState([])
 
     useEffect(() => {
         const dbRef = ref(database, 'MONITOR'); // Đường dẫn đến dữ liệu bạn muốn đọc
@@ -22,11 +41,45 @@ const Home = () => {
             console.log(dataObject);
         });
 
+        const dbRef1 = ref(database, 'LOG')
+        onValue(dbRef1, (snapshot) => {
+            const dataObject = snapshot.val();
+            let dataNew = [];
+            try {
+                dataNew = Object.keys(dataObject).map((key, i) => ({
+                    key,
+                    no: i + 1,
+                    timestamp: dataObject[key].timestamp,
+                    fire: dataObject[key].fire,
+                }));
+                const reversedData = dataNew.slice().reverse();
+                setDataTable(reversedData)
+            } catch (error) {
+                message.error('Error processing data');
+                console.error('Error processing data:', error);
+            }
+
+        });
         // Clean up subscription on unmount
         return () => unsubscribe();
 
     }, []);
+    useEffect(() => {
 
+        const interval = setInterval(() => {
+            const timestamp = new Date().toISOString().replace(/[:.]/g, '-');
+            const timestamp2 = new Date().toISOString();
+
+
+            set(ref(database, 'LOG/' + timestamp), {
+                timestamp: timestamp2,
+                fire: data?.O_Baochay?.data === "1" ? "Cảnh báo" : "Bình thường"
+            })
+
+        }, 60 * 1000); // 15 phút
+
+        return () => clearInterval(interval);  // Cleanup interval khi component unmount
+    }, [data]);
     const handlerClick = () => {
         const dbRef = ref(database, 'MONITOR/O_Baochay/data');
         const dbRef2 = ref(database, 'CONTROL/NNKC/data');
@@ -94,8 +147,9 @@ const Home = () => {
                     />
                 </>) : ""}
             </div>
-
+            <Table dataSource={dataTable} columns={columns} pagination={{ pageSize: 8 }} />;
         </div>
+
     </>);
 }
 
