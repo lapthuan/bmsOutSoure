51c4dc9a0a26c5f1428766740468de0b3e7cfa18
lapthuan <lapthuan0805@gmail.com>
2024-05-25 16:41:48 +0700

    a

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -69,16 +69,26 @@ const Home = () => {
         const interval = setInterval(() => {
             const timestamp = new Date().toISOString().replace(/[:.]/g, '-');
             const timestamp2 = new Date().toISOString();
+            if (data?.O_Baochay?.data === "1") {
+                if (data?.O_CT?.data === "1") {
 
+                    set(ref(database, 'LOG/' + timestamp), {
+                        timestamp: timestamp2,
+                        fire: "Có cháy"
+                    })
+                } else {
+                    set(ref(database, 'LOG/' + timestamp), {
+                        timestamp: timestamp2,
+                        fire: "Cảnh báo cháy"
+                    })
+                }
 
-            set(ref(database, 'LOG/' + timestamp), {
-                timestamp: timestamp2,
-                fire: data?.O_Baochay?.data === "1" ? "Cảnh báo" : "Bình thường"
-            })
+            }
 
         }, 60 * 1000); // 15 phút
 
         return () => clearInterval(interval);  // Cleanup interval khi component unmount
+
     }, [data]);
     const handlerClick = () => {
         const dbRef = ref(database, 'MONITOR/O_Baochay/data');
